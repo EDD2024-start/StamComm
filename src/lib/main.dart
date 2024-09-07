146c7c2d720dc1d599eb76b49aa855cdb9e6602b
Tsukumon3 <aniueooo@gmail.com>
2024-09-07 21:57:19 +0900

    [wip]

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,24 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'firebase_options.dart';
-<<<<<<< .merge_file_UfZvjx
-import 'view/display_map.dart'; // display_map.dartをインポート
-=======
 import 'reset_password_screen.dart';
 import 'validation.dart';
 import 'component/navigate.dart';
->>>>>>> .merge_file_PmZW5D
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(const LoginSample());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class LoginSample extends StatelessWidget {
+  const LoginSample({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -265,7 +263,6 @@ class _LoginPageState extends State<LoginPage> {
           ),
         ),
       ),
-      home: const DisplayMap(), // DisplayMapウィジェットを表示
     );
   }
 }
