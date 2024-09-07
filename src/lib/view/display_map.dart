892bcba32a55108800c46046622422b5b9b51c1e
kuroishihiromu <kuroishihiromu@gmail.com>
2024-09-07 21:35:15 +0900

    [upgrade] 地図にピンが表示される機能を追加
    jsonから座標を取得して、その座標にピンを表示。

--- src/lib/view/display_map.dart
+++ src/lib/view/display_map.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:platform_maps_flutter/platform_maps_flutter.dart';
+import 'package:StamComm/models/location_data.dart';
 
 class DisplayMap extends StatefulWidget {
   DisplayMap({Key? key}) : super(key: key);
@@ -11,6 +13,35 @@ class DisplayMap extends StatefulWidget {
 
 class _DisplayMapState extends State<DisplayMap> {
   late PlatformMapController _mapController;
+  Set<Marker> _markers = {};
+
+  @override
+  void initState(){
+    super.initState();
+    _loadLocationData();
+  }
+
+  void _loadLocationData() {
+    // /src/assets/data/sample_data.jsonから位置情報を読み込む
+    const String jsonDataPath = 'assets/data/sample_data.json';
+    DefaultAssetBundle.of(context).loadString(jsonDataPath).then((String data) {
+      final List<dynamic> jsonList = json.decode(data);
+      final List<LocationData> locations = jsonList.map((json) => LocationData.fromJson(json)).toList();
+      setState(() {
+        _markers = locations.map((location){
+          return Marker(
+            markerId: MarkerId(location.id),
+            position: LatLng(location.latitude, location.longitude),
+            infoWindow: InfoWindow(
+              title: location.name,
+              snippet: location.descriptionText,
+            ),
+          );
+        }).toSet();
+      });
+    });
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +60,7 @@ class _DisplayMapState extends State<DisplayMap> {
         ),
         myLocationEnabled: true,
         myLocationButtonEnabled: true,
+        markers: _markers,
       ),
     );
   }
