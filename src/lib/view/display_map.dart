7ba37ded77a5771697927d2c6f718831fde4c65c
kuroishihiromu <kuroishihiromu@gmail.com>
2024-09-07 18:45:49 +0900

    [upgrade] マップ起動時に、現在位置周辺が表示されるように修正

--- src/lib/view/display_map.dart
+++ src/lib/view/display_map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:platform_maps_flutter/platform_maps_flutter.dart';
 
 class DisplayMap extends StatefulWidget {
@@ -9,63 +10,37 @@ class DisplayMap extends StatefulWidget {
 }
 
 class _DisplayMapState extends State<DisplayMap> {
+  late PlatformMapController _mapController;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Display Map'),
       ),
-      body: MapWidget(),
-    );
-  }
-}
-
-class MapWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: double.infinity, // 高さを指定
-      width: double.infinity, // 幅も指定
-      child: PlatformMap(
-        initialCameraPosition: const CameraPosition(
-          target: LatLng(47.6, 8.8796),
-          zoom: 16,
-        ),
-        markers: <Marker>{
-          Marker(
-            markerId: MarkerId('marker_1'),
-            position: const LatLng(47.6, 8.8796),
-            consumeTapEvents: true,
-            infoWindow: const InfoWindow(
-              title: 'PlatformMarker',
-              snippet: "Hi I'm a Platform Marker",
-            ),
-            onTap: () {
-              // print("Marker tapped");
-            },
-          ),
+      body: PlatformMap(
+        onMapCreated: (controller) async {
+          _mapController = controller;
+          _moveCameraToCurrentLocation();
         },
+        initialCameraPosition: CameraPosition(
+          target: LatLng(0, 0),
+          zoom: 15,
+        ),
         myLocationEnabled: true,
         myLocationButtonEnabled: true,
-        // onTap: (location) => print('ontap: $location'),
-        // onCameraMove: (cameraUpdate) => print('onCameraMove: $cameraUpdate'),
-        compassEnabled: true,
-        onMapCreated: (controller) {
-          Future<dynamic>.delayed(const Duration(seconds: 2)).then(
-            (dynamic _) {
-              controller.animateCamera(
-                CameraUpdate.newCameraPosition(
-                  const CameraPosition(
-                    bearing: 270.0,
-                    target: LatLng(51.5160895, -0.1294527),
-                    // tilt: 30.0,
-                    // zoom: 18,
-                  ),
-                ),
-              );
-            },
-          );
-        },
+      ),
+    );
+  }
+
+  // カメラを現在地に移動する関数
+  void _moveCameraToCurrentLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    
+    _mapController.moveCamera(
+      CameraUpdate.newLatLng(
+        LatLng(position.latitude, position.longitude),
       ),
     );
   }
