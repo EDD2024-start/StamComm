b0652c0f1789639412f6047ff51ec83c53b874d5
kuroishihiromu <kuroishihiromu@gmail.com>
2024-09-11 19:33:20 +0900

    [update] android用のマップの設定を追加

--- src/lib/view/display_map.dart
+++ src/lib/view/display_map.dart
@@ -1,27 +1,41 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
+import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
 
 class DisplayMap extends StatefulWidget {
+  const DisplayMap({super.key});
   @override
-  _DisplayMapState createState() => _DisplayMapState();
+  DisplayMapState createState() => DisplayMapState();
 }
 
-class _DisplayMapState extends State<DisplayMap> {
-  CameraPosition _initialLocation = CameraPosition(target: LatLng(0.0, 0.0));
+class DisplayMapState extends State<DisplayMap> {
+  CameraPosition _initialLocation = const CameraPosition(target: LatLng(0.0, 0.0));
   late GoogleMapController mapController;
 
   @override
   void initState() {
     super.initState();
+    _initializeMapRenderer();
+  }
+
+  void _initializeMapRenderer() {
+    final GoogleMapsFlutterPlatform mapsImplementation = GoogleMapsFlutterPlatform.instance;
+    if (mapsImplementation is GoogleMapsFlutterAndroid) {
+      mapsImplementation.useAndroidViewSurface = true;
+    }
+  }
+  
+  @override
+  void dispose() {
+    mapController.dispose();
+    super.dispose(); 
   }
 
   void _getCurrentLocation() async {
     try {
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.high);
+      Position position = await Geolocator.getCurrentPosition();
 
       setState(() {
         _initialLocation = CameraPosition(
@@ -42,10 +56,10 @@ class _DisplayMapState extends State<DisplayMap> {
   @override
   Widget build(BuildContext context) {
     // 画面の幅と高さを決定する
-    var height = MediaQuery.of(context).size.height;
-    var width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
 
-    return Container(
+    return SizedBox(
       height: height,
       width: width,
       child: Scaffold(
@@ -69,78 +83,3 @@ class _DisplayMapState extends State<DisplayMap> {
     );
   }
 }
-
-
-// class DisplayMap extends StatefulWidget {
-//   DisplayMap({Key? key}) : super(key: key);
-
-//   @override
-//   _DisplayMapState createState() => _DisplayMapState();
-// }
-
-// class _DisplayMapState extends State<DisplayMap> {
-//   late PlatformMapController _mapController;
-//   Set<Marker> _markers = {};
-
-//   @override
-//   void initState(){
-//     super.initState();
-//     _loadLocationData();
-//   }
-
-//   void _loadLocationData() {
-//     // /src/assets/data/sample_data.jsonから位置情報を読み込む
-//     const String jsonDataPath = 'assets/data/sample_data.json';
-//     DefaultAssetBundle.of(context).loadString(jsonDataPath).then((String data) {
-//       final List<dynamic> jsonList = json.decode(data);
-//       final List<LocationData> locations = jsonList.map((json) => LocationData.fromJson(json)).toList();
-//       setState(() {
-//         _markers = locations.map((location){
-//           return Marker(
-//             markerId: MarkerId(location.id),
-//             position: LatLng(location.latitude, location.longitude),
-//             infoWindow: InfoWindow(
-//               title: location.name,
-//               snippet: location.descriptionText,
-//             ),
-//           );
-//         }).toSet();
-//       });
-//     });
-
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Display Map'),
-//       ),
-//       body: PlatformMap(
-//         onMapCreated: (controller) async {
-//           _mapController = controller;
-//           _moveCameraToCurrentLocation();
-//         },
-//         initialCameraPosition: CameraPosition(
-//           target: LatLng(0, 0),
-//           zoom: 15,
-//         ),
-//         myLocationEnabled: true,
-//         myLocationButtonEnabled: true,
-//         markers: _markers,
-//       ),
-//     );
-//   }
-
-//   // カメラを現在地に移動する関数
-//   void _moveCameraToCurrentLocation() async {
-//     Position position = await Geolocator.getCurrentPosition(
-//         desiredAccuracy: LocationAccuracy.high);
-    
-//     _mapController.moveCamera(
-//       CameraUpdate.newLatLng(
-//         LatLng(position.latitude, position.longitude),
-//       ),
-//     );
-//   }
-// }
