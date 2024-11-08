db47fb0aa01c2b266e8b6d9c0111c92fa9e4808b
kuroishihiromu <kuroishihiromu@gmail.com>
2024-11-09 05:26:53 +0900

    [upgrade]バッジロゴの変更。表示の修正

--- src/lib/view/home.dart
+++ src/lib/view/home.dart
@@ -63,88 +63,102 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('StamComm'),
-        centerTitle: true, // タイトルを中央に配置
-      ),
-      body: data.isEmpty
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: data.length,
-              itemBuilder: (context, index) {
-                final item = data[index];
-                final isCompleted = completedEventIds.contains(item['id']);
-                return Column(
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('StamComm'),
+      centerTitle: true, // タイトルを中央に配置
+    ),
+    body: data.isEmpty
+        ? Center(child: CircularProgressIndicator())
+        : PageView.builder(
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              final item = data[index];
+              final isCompleted = completedEventIds.contains(item['id']);
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.9,
-                      height: 70,
-                      child: Stack(
-                        children: [
-                          // ElevatedButton
-                          ElevatedButton(
-                            onPressed: () {
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => NavigateApp(
-                                    initialPageIndex: 1,
-                                    eventId: item['id'],
-                                  ),
-                                ),
-                              );
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: isCompleted
-                                  ? const Color.fromARGB(255, 170, 170, 170)
-                                  : const Color.fromARGB(255, 255, 255, 255),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5), // 角の丸さを設定
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      height: MediaQuery.of(context).size.height * 0.7,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => NavigateApp(
+                                initialPageIndex: 1,
+                                eventId: item['id'],
                               ),
                             ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[
-                                Padding(
-                                  padding: const EdgeInsets.fromLTRB(
-                                      0, 0, 0, 0), // 上下左右に同じ余白を設定
-                                  child: Image.network(
-                                    item['description_image_url'], // Supabaseから取得した画像URL
-                                    width: 50,
-                                    height: 50,
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Text(
-                                    item['name'],
-                                    textAlign: TextAlign.right,
-                                    softWrap: false,
-                                  ),
-                                ),
-                              ],
-                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5), // 角の丸さを設定
                           ),
-                          // 完了状態の場合、completed.pngを重ねる
-                          if (isCompleted)
-                            Positioned.fill(
-                              child: Align(
-                                alignment: Alignment.center,
-                                child: Image.asset(
-                                  'assets/images/complete.png', // 画像のパス
-                                  width: 100,
-                                  height: 100,
-                                ),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 10, 0, 5),
+                              child: Image.network(
+                                item['description_image_url'], // Supabaseから取得した画像URL
+                                width: MediaQuery.of(context).size.width * 0.3,
+                                height: MediaQuery.of(context).size.height * 0.3,
                               ),
                             ),
-                        ],
+                            Text(
+                              item['name'],
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              item['description_text'],
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(fontSize: 14),
+                            ),
+                            if (isCompleted) ...[
+                              const SizedBox(height: 20),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset(
+                                    'assets/images/complete.png', // 完了画像のパス
+                                    width: 80,
+                                    height: 80,
+                                  ),
+                                  const SizedBox(width: 10),
+                                  const Expanded(
+                                    child: Text(
+                                      'あなたはこのイベントのスタンプを全て集めました！',
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ],
+                        ),
                       ),
                     ),
-                    SizedBox(height: 10), // ボタン間のスペース
                   ],
-                );
-              },
-            ),
-    );
-  }
+                ),
+              );
+            },
+          ),
+  );
+}
 }
