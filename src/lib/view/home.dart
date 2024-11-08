0b35310e9cbdafbda6915de9a7d87096bec5f44b
kuroishihiromu <kuroishihiromu@gmail.com>
2024-11-09 00:56:55 +0900

    [upgrade] ホーム画面で、コンプリートしたイベントの描画を取得済みに変更

--- src/lib/view/home.dart
+++ src/lib/view/home.dart
@@ -10,11 +10,13 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final supabase = Supabase.instance.client;
   List<dynamic> data = [];
+  List<String> completedEventIds = []; 
 
   @override
   void initState() {
     super.initState();
     fetchData();
+    fetchCompletedEvents();
   }
 
   void fetchData() async {
@@ -27,6 +29,36 @@ class _HomePageState extends State<HomePage> {
       // エラーハンドリング
       print(response);
     }
+  }
+
+  Future<List<String>> getCompletedEvents(String userId) async {
+    final response = await supabase.rpc(
+      'get_completed_events',
+      params: {'user_id': userId},
+    );
+
+    if (response != null) {    
+      // 取得したデータをイベントIDのリストとして返す
+      // final data = response;
+      List<dynamic> events = response;
+      List<String> completedEventIds = events.map((event) {
+        return event['event_id'].toString();
+      }).toList();
+      print('Completed events:' + completedEventIds.toString());
+      return completedEventIds;
+    } else {
+      print('Error fetching completed events:' + response.toString());
+      return [];
+    }
+  }
+
+  Future<void> fetchCompletedEvents() async {
+    final userId = supabase.auth.currentUser?.id;
+    if (userId == null) {
+      return;
+    }
+    completedEventIds = await getCompletedEvents(userId);
+    setState(() {});
   }
 
   @override
@@ -42,50 +74,70 @@ class _HomePageState extends State<HomePage> {
               itemCount: data.length,
               itemBuilder: (context, index) {
                 final item = data[index];
+                final isCompleted = completedEventIds.contains(item['id']);
                 return Column(
                   children: [
                     SizedBox(
                       width: MediaQuery.of(context).size.width * 0.9,
                       height: 70,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => NavigateApp(
-                                  initialPageIndex: 1, eventId: item['id']),
+                      child: Stack(
+                        children: [
+                          // ElevatedButton
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => NavigateApp(
+                                    initialPageIndex: 1,
+                                    eventId: item['id'],
+                                  ),
+                                ),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: isCompleted
+                                  ? const Color.fromARGB(255, 170, 170, 170)
+                                  : const Color.fromARGB(255, 255, 255, 255),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5), // 角の丸さを設定
+                              ),
                             ),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                              const Color.fromARGB(255, 217, 217, 217),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5), // 角の丸さを設定
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.fromLTRB(
+                                      0, 0, 0, 0), // 上下左右に同じ余白を設定
+                                  child: Image.network(
+                                    item['description_image_url'], // Supabaseから取得した画像URL
+                                    width: 50,
+                                    height: 50,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Text(
+                                    item['name'],
+                                    textAlign: TextAlign.right,
+                                    softWrap: false,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(
-                                  0, 0, 0, 0), // 上下左右に同じ余白を設定
-                              child: Image.network(
-                                item[
-                                    'description_image_url'], // Supabaseから取得した画像URL
-                                width: 50,
-                                height: 50,
+                          // 完了状態の場合、completed.pngを重ねる
+                          if (isCompleted)
+                            Positioned.fill(
+                              child: Align(
+                                alignment: Alignment.center,
+                                child: Image.asset(
+                                  'assets/images/complete.png', // 画像のパス
+                                  width: 100,
+                                  height: 100,
+                                ),
                               ),
                             ),
-                            Expanded(
-                              child: Text(
-                                item['name'],
-                                textAlign: TextAlign.right,
-                                softWrap: false,
-                              ),
-                            ),
-                          ],
-                        ),
+                        ],
                       ),
                     ),
                     SizedBox(height: 10), // ボタン間のスペース
